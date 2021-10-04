15ef23bc7c0770db6aa89878575115b10d31b7e8
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-04 14:16:17 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express');
 //route to request data
-const { animals } = require('./db/notes');
+const { notes } = require('./db/notes');
 //instantiate the server with express
 const app = express();
 
+//filter function
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if (query.) {
+      filteredResults = filteredResults.filter(notes => notes. === notes.);
+    }
+    if (query.) {
+      filteredResults = filteredResults.filter(notes => notes. === query.);
+    }
+    if (query.) {
+      filteredResults = filteredResults.filter(notes => notes. === notes.;
+    }
+    return filteredResults;
+  }
+
 //get route
 app.get('/api/notes', (req, res) => {
-    res.send('Notes');
+    let results = notes;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
   });
 
 //listen method
